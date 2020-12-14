1aee6cda48ea7af2ebbe3febaf5fa2914ff6abbf
benttol <75014765+benttol@users.noreply.github.com>
2020-12-14 19:43:53 +0700

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,19 @@
+const help = () => {
+	return '*[ WhatsApp Bot *Y** *]*\n\n' +
+	'*Note : Semua Fitur menggunakan (#) dan tidak menggunakan [ ] atau ( )*\n\n' +
+	'Creator\n' +
+	'*➣ #toimg*\n' +
+	'*➣ #sticker*\n' +
+	'*➣ #ttp [teks]*\n' +
+	'*➣ #gifsticker*\n\n' +
+	'Fun Commands' + 
+	'*➣ #ping*\n' +
+	'*➣ #wait*\n' +
+	'*➣ #meme*\n' +
+	'*➣ #ttp [teks]*\n' +
+	'*➣ #nulis [teks]*\n' +
+	'*➣ #memeindo*\n\n' +
+	'*Terimakasih sudah membaca dalam hati walau aku tau hatimu bukan untukku.*' +
+}
+
+exports.help = help
